0c617aa4fc0a4df61839a25f182d734466bb6ed0
Hector Miuler Malpica Gallegos <miuler@gmail.com>
2021-01-19 16:43:42 -0500

    mejora en el componente de busqueda

--- front/src/main/scala/medical/SearchSection.scala
+++ front/src/main/scala/medical/SearchSection.scala
@@ -1,49 +1,76 @@
 package medical
 
 import com.raquo.laminar.api.L._
+import com.raquo.laminar.nodes.ReactiveHtmlElement
 import org.scalajs.dom
+import org.scalajs.dom.html
+import org.scalajs.dom.html.Table
 import org.scalajs.dom.raw.HTMLTableCellElement
 
-import scala.scalajs.js.|
 
 object SearchSection {
   private val patients = Var(List[PatientBasic]())
 
   def apply(patientBasicWriteBus: WriteBus[Option[PatientBasic]]): HtmlElement = {
-    val searchInput = input(
+    val eventBus = new EventBus[dom.html.Element]
+    val input = searchInput(eventBus)
+
+    section(
+      div(
+        className := "container input-group mb-3",
+        input,
+        searchButton(eventBus, input),
+      ),
+      br(),
+      searchTable(patientBasicWriteBus),
+    )
+  }
+
+  def searchInput(eventBus: EventBus[dom.html.Element]): ReactiveHtmlElement[html.Input] = {
+    def obsInput(input: Input) = Observer[dom.KeyboardEvent](onNext = { event =>
+      val text = input.ref.value
+      val search = text.trim.nonEmpty
+      scribe.info("Buscar!")
+      if (search) {
+        eventBus.writer.onNext(div(text).ref)
+        patients.update(_ :+ PatientBasic(text, text))
+      }
+    })
+
+    input(
       "searchInput",
       className := "form-control",
       placeholder := "Nombre del paciente",
       autoFocus(true),
+      inContext(thisNode => onKeyPress.filter(e => e.keyCode == dom.ext.KeyCode.Enter) --> obsInput(thisNode))
     )
-    val searchButton = button("buscar", className := "btn btn-outline-primary")
-    val result = div(idAttr := "result")
-    val eventBus = new EventBus[dom.html.Element]
+  }
 
-    //    val obs = Observer[dom.MouseEvent | dom.KeyboardEvent](onNext = { event =>
-    //      scribe.info(s"event $event")
-    //      scribe.info(searchInput.ref.value)
-    //    })
-    val obs = Observer[dom.MouseEvent | dom.KeyboardEvent](onNext = { event =>
-      val text = searchInput.ref.value
-      val search = event match {
-        case mouseEvent: dom.MouseEvent => scribe.debug(s"mouseEvent: $mouseEvent"); !text.trim.isEmpty
-        case keyboardEvent: dom.KeyboardEvent => {
-          scribe.debug(s"keyboardEvent: ${keyboardEvent.keyCode}")
-          text.trim.nonEmpty
-        }
-        case _ => scribe.debug(s"event: $event"); false
-      }
-      if (search) {
+  def searchButton(eventBus: EventBus[dom.html.Element], input: ReactiveHtmlElement[html.Input]): ReactiveHtmlElement[html.Button] = {
+
+    val obsButton = Observer[dom.MouseEvent](onNext = { event =>
+      val text = input.ref.value
+      scribe.info(s"mouseEvent: $event")
+      val search = text.trim.isEmpty
+      scribe.info(s"mouseEvent: $event")
+      if (!search) {
         scribe.info("Buscar!")
         eventBus.writer.onNext(div(text).ref)
         patients.update(_ :+ PatientBasic(text, text))
       }
     })
-    val onHistory = Observer[dom.MouseEvent](onNext = {event =>
+
+    button(
+      "buscar",
+      className := "btn btn-outline-primary",
+      onClick --> obsButton,
+    )
+  }
+
+  def searchTable(patientBasicWriteBus: WriteBus[Option[PatientBasic]]): ReactiveHtmlElement[Table] = {
+    val obsHistory = Observer[dom.MouseEvent](onNext = {event =>
       scribe.info(s"event: ${event}")
       scribe.info(s"1: ${event.target.isInstanceOf[HTMLTableCellElement]}")
-      scribe.info(s"2: ${event.target.asInstanceOf[HTMLTableCellElement]}")
       val target = event.target.asInstanceOf[HTMLTableCellElement]
       scribe.info(s"event.id: ${target.id}")
       val i = target.id.toInt
@@ -52,25 +79,11 @@ object SearchSection {
       patientBasicWriteBus.onNext(Some(patient))
     })
 
-    searchButton.amend(onClick --> obs)
-    searchInput.amend(onKeyPress.filter(e => e.keyCode == dom.ext.KeyCode.Enter) --> obs)
-    //    searchInput.amend(inContext { thisNode => onInput.map(_ => thisNode.ref.value) --> obs } )
-    //    searchInput.amend(inContext { thisNode => onKeyPress.map(_ => thisNode.ref.value) --> obs } )
-
-    section(
-      div(
-        className := "container input-group mb-3",
-        searchInput,
-        searchButton,
-      ),
-      br(),
-//      child <-- eventBus.events,
-      table(
-        className := "container table table-hover",
-        tbody(
-          children <-- patients.signal.map(e => e.zipWithIndex.map(i => tr(td(idAttr := s"${i._2}", i._1.name, onClick --> onHistory)))),
-        )
-      ),
+    table(
+      className := "container table table-hover",
+      tbody(
+        children <-- patients.signal.map(e => e.zipWithIndex.map(i => tr(td(idAttr := s"${i._2}", i._1.name, onClick --> obsHistory)))),
+      )
     )
   }
 }
