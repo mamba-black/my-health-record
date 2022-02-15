33642e19cc6152d3a401956c5fccb49b626d120a
Hector Miuler Malpica Gallegos <miuler@gmail.com>
2022-02-14 23:05:26 -0500

    ✨ feat: cambiando getById para retornar un Future

--- front/src/main/scala/medical/domain/repository/PatientRepository.scala
+++ front/src/main/scala/medical/domain/repository/PatientRepository.scala
@@ -1,13 +1,15 @@
 package medical.domain.repository
 
 import com.raquo.airstream.core.{ EventStream, Observer }
-import medical.api.patientapi.PatientReply
+import medical.api.patientapi.{ FullPatientReply, PatientReply }
 import medical.domain.Patient
+
+import scala.concurrent.Future
 
 trait PatientRepository {
   def findByName(name: String): EventStream[PatientReply]
 
-  def getById(patientId: String, patientWriter: Observer[Option[Patient]]): Unit
+  def getById(patientId: String): Future[FullPatientReply]
 
   def save(patient: Patient, patientWriter: Observer[Option[Patient]]): Unit
 }
--- front/src/main/scala/medical/infrastructure/repository/PatientRepositoryImpl.scala
+++ front/src/main/scala/medical/infrastructure/repository/PatientRepositoryImpl.scala
@@ -3,15 +3,15 @@ package medical.infrastructure.repository
 import com.raquo.airstream.core.{ EventStream, Observer }
 import com.raquo.airstream.eventbus.EventBus
 import io.grpc.stub.StreamObserver
-import medical.api.patientapi.{ PatientApiGrpcWeb, PatientIdRequest, PatientReply, PatientRequest }
-import medical.domain.{ ContactPoint, HumanName, Patient, SystemContactPoint }
+import medical.api.patientapi.*
+import medical.domain.Patient
 import medical.domain.repository.PatientRepository
 import scalapb.grpc.Channels
 import scribe.*
-import concurrent.ExecutionContext.Implicits.global
+
+import scala.concurrent.Future
 //import scala.scalajs.concurrent.JSExecutionContext.Implicits.queue
 
-import java.time.LocalDate
 import scala.scalajs.js.timers.setTimeout
 
 private[infrastructure] class PatientRepositoryImpl extends PatientRepository {
@@ -37,37 +37,22 @@ private[infrastructure] class PatientRepositoryImpl extends PatientRepository {
     eventBus.events
   }
 
-  override def getById(patientId: String, patientWriter: Observer[Option[Patient]]): Unit = {
+  override def getById(patientId: String): Future[FullPatientReply] = {
     debug("1")
     patientApi
       .getFullPatient(PatientIdRequest.of(patientId))
-      .map(s => {
-        debug("2")
-        info(s"s: $s")
-        val patient = new Patient(
-          patientId,
-          new HumanName("Malpica2", "Gallegos2", Seq("Hector2", "Miuler2")),
-          true,
-          LocalDate.of(1979, 10, 13),
-          Seq(new ContactPoint(SystemContactPoint.PHONE, "+51993990103")),
-        )
-        patientWriter.onNext(Some(patient))
-        debug("TIMEOUT")
-      })
-    debug("3")
 
-    setTimeout(2000) {
-      val patient = new Patient(
-        patientId,
-        new HumanName("Malpica", "Gallegos", Seq("Hector", "Miuler")),
-        true,
-        LocalDate.of(1979, 10, 13),
-        Seq(new ContactPoint(SystemContactPoint.PHONE, "993990103")),
-      )
-      patientWriter.onNext(Some(patient))
-      debug("TIMEOUT")
-    }
-    ()
+    // setTimeout(2000) {
+    //  val patient = new Patient(
+    //    patientId,
+    //    new HumanName("Malpica", "Gallegos", Seq("Hector", "Miuler")),
+    //    true,
+    //    LocalDate.of(1979, 10, 13),
+    //    Seq(new ContactPoint(SystemContactPoint.PHONE, "993990103")),
+    //  )
+    //  patientWriter.onNext(Some(patient))
+    //  debug("TIMEOUT")
+    // }
   }
 
   override def save(patient: Patient, patientWriter: Observer[Option[Patient]]): Unit = {
