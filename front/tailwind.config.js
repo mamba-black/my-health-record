5ec6d8d0ae29b7559dc28146e9af3222a06f99ef
Hector Miuler Malpica Gallegos <miuler@gmail.com>
2022-03-28 11:14:23 -0500

    ✨ feat(dialog): Probando nuevo dialogo

--- front/tailwind.config.js
+++ front/tailwind.config.js
@@ -1,11 +1,14 @@
 module.exports = {
+  mode: 'jit',
   purge: {
     enabled: false,
     content: [
-      '/home/miuler/proyectos/Medical/frontend/my-health-record.ui/front/target/scala-3.1.1/my-health-record-ui-fastopt/main.js'
+      // '/home/miuler/proyectos/Medical/frontend/my-health-record.ui/front/target/scala-3.1.1/my-health-record-ui-fastopt/main.js',
+      '/home/miuler/proyectos/Medical/frontend/my-health-record.ui/front/target/scala-3.1.1/scalajs-bundler/main/my-health-record-ui-fastopt-bundle.js',
+      './my-health-record-ui-fastopt-bundle.js'
     ]
   },
-  darkMode: false, // or 'media' or 'class'
+  // darkMode: false, // or 'media' or 'class'
   theme: {
     extend: {},
   },
