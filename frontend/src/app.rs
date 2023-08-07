2f70612c8f67abd6e6ce9ce9e0041d65da4b7a35
Hector Miuler Malpica Gallegos <miuler@gmail.com>
2023-08-06 22:32:17 -0500

    ✨ feat: Agregando la libreria de google para su oauth y su correspondiente binding

--- frontend/src/app.rs
+++ frontend/src/app.rs
@@ -1,5 +1,8 @@
+use gloo_storage::Storage;
+use log::info;
 use yew::prelude::*;
 use yew_router::prelude::*;
+// use medical_front::auth2;
 
 use medical_front::infrastructure::ui::app_state_context::{AppState, AppStateContext};
 use medical_front::infrastructure::ui::pages::history_detail::HistoryDetail;
@@ -7,6 +10,7 @@ use medical_front::infrastructure::ui::pages::home::Home;
 use medical_front::infrastructure::ui::pages::not_found::NotFound;
 use medical_front::infrastructure::ui::pages::search::Search;
 use medical_front::infrastructure::ui::route::{PrivateRoute, PublicRoute};
+use medical_front::{initCodeClient, CodeClientConfig};
 
 #[function_component]
 pub fn App() -> Html {
@@ -28,16 +32,42 @@ fn public_switch(route: PublicRoute) -> Html {
         PublicRoute::Home => html! { <Home /> },
         PublicRoute::About => html! { <Home /> },
         PublicRoute::PrivatesRoot | PublicRoute::Privates => {
-            html! { <Switch<PrivateRoute> render={private_switch}/> }
+            html! { <PrivateSwitch /> }
         }
         PublicRoute::NotFound => html! { <NotFound /> },
     }
 }
 
+#[function_component]
+fn PrivateSwitch() -> Html {
+    let a = gloo_storage::LocalStorage::get::<String>("access_token");
+    if a.is_ok() {
+        info!("token: {}", a.unwrap());
+        html! { <Switch<PrivateRoute> render={private_switch}/> }
+    } else {
+        info!("No hay token");
+        // initCodeClient(CodeClientConfig::new (
+        //     "937186309482-uc7dm6bc6o6p3disa546hq25n8dbov42.apps.googleusercontent.com".to_string(),
+        //     "https://www.googleapis.com/auth/calendar.readonly".to_string(),
+        //     "redirect".to_string(),
+        //     "https://your.domain/code_callback_endpoint".to_string(),
+        // ));
+        html! { <Switch<PrivateRoute> render={private_switch}/> }
+    }
+
+    // let auth = auth2::getAuthInstance();
+    // if auth.isSignedIn() {
+    //     info!("Esta logeado contra google");
+    // } else {
+    //     info!("Tienes que logearte contra google");
+    // }
+    // auth.signIn();
+}
+
 fn private_switch(route: PrivateRoute) -> Html {
     match route {
         PrivateRoute::Histories => html! { <Search /> },
-        PrivateRoute::HistoryDetail { id } => html! { <HistoryDetail {id} /> },
+        PrivateRoute::HistoryDetail { id } => html! { <HistoryDetail id={id.clone()} /> },
         PrivateRoute::NotFound | PrivateRoute::NotFound2 => html! { <NotFound /> },
     }
 }
