a5ccb0bb91ed2a1691e2bce0e6e0d48feb88e44d
Hector Miuler Malpica Gallegos <miuler@gmail.com>
2025-07-12 03:55:17 -0500

    feat: Se agrego un 404

---
+++ frontendts/src/routes/[...404]/+page.svelte
@@ -0,0 +1,36 @@
+<script lang="ts">
+  import svelteLogo from "$lib/svelte.svg";
+  import {base} from "$app/paths";
+</script>
+
+<!-- =============================================================== -->
+<hr />
+<center>
+  <table>
+    <tbody>
+    <tr>
+      <td><a href="{base}/search"><img src="{base}/vite.svg" class="logo" alt="Vite Logo" /></a></td>
+      <td>
+        <img src={svelteLogo} class="logo svelte" alt="Svelte Logo" />
+      </td>
+    </tr>
+    </tbody>
+  </table>
+</center>
+<!-- =============================================================== -->
+<style>
+  .logo {
+    height: 6em;
+    padding: 1.5em;
+    will-change: filter;
+    transition: filter 300ms;
+  }
+
+  .logo:hover {
+    filter: drop-shadow(0 0 2em #646cffaa);
+  }
+
+  .logo.svelte:hover {
+    filter: drop-shadow(0 0 2em #ff3e00aa);
+  }
+</style>
